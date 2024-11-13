241979b2c2666aed0f0296b4aff0852ea182cd85
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-13 19:22:27 -0300

    paginas-e-rotas

--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styled';
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-export const Button = ({ variant = 'primary' }: ButtonProps) => {
-  return <ButtonContainer variant={variant}>Button</ButtonContainer>;
-};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from './styles';
+
+import { Scroll, Timer } from 'phosphor-react';
+import { NavLink } from 'react-router-dom';
+import LogoIgnite from '../../assets/logo-ignite.svg';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <img src={LogoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} alt="Timer" />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} alt="Histórico" />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+};
